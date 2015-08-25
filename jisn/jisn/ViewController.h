bec65087f10997231183f41a82e17f1db51d8f2a
tarena001 <tarena01@Sherman-2.local>
2015-08-25 16:31:45 +0800

    创建了一个新的工程是：哇咔咔 哈哈

---
+++ jisn/jisn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jisn
+//
+//  Created by tarena001 on 15-8-25.
+//  Copyright (c) 2015年 tarena1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jisn/jisn/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  jisn
+//
+//  Created by tarena001 on 15-8-25.
+//  Copyright (c) 2015年 tarena1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
